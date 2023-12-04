d21efee542b9c44ee9d0fd66b18a7b72e9999fb2
shkimhi <ajfqhksh1@gmail.com>
2023-12-04 22:21:53 +0900

    Initial commit

---
+++ src/main/java/com/talktosql/talktosqlweb/TalktosqlwebApplication.java
@@ -0,0 +1,13 @@
+package com.talktosql.talktosqlweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalktosqlwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalktosqlwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/talktosql/talktosqlweb/TalktosqlwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talktosql.talktosqlweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalktosqlwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
